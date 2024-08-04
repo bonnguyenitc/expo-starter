56c13d755026e637fec773293f3f2bf4d0d3da47
thoaintbap <thoaint@bap.jp>
2024-08-04 22:31:55 +0700

    feat: Add axios instance for API calls

---
+++ src/utils/axios.ts
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const axiosInstance = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+})
+
+export default axiosInstance
